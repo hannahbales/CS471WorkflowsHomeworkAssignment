fcf2e606457e09e20d91558ca2153d2f6a9d7c21
Hannah Bales <133197883+hannahbales@users.noreply.github.com>
2024-05-07 16:42:56 -0700

    Closes #6 (#10)

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        //this is a main method
+    }
+
+    public void name() {
+        //this is a new method
+    }
+
+    public void name2() {
+        //this is another new method
+    }
+}
